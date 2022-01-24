ff8d98783cfc081b243d2b665a42be991999195a
Lucas Araripe <pedro.lucx@gmail.com>
2022-01-24 11:22:50 -0300

    Starting the use of styled-components

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import LogoText from './styles';
+
+export default function Home(){
+    return (
+        <LogoText>
+            Git-Search
+        </LogoText>
+    )
+}
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const logoText = styled.div`
+    
+    color: red;
+
+    font-size: 2.5rem;
+`
+
+export default logoText;
