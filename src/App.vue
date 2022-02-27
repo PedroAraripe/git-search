fcea29fd23abd040bd90099374de5443551fa5b3
Lucas Araripe <pedro.lucx@gmail.com>
2022-02-27 14:20:10 -0300

    Implementing no user found text

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Shippori Antique B1", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
